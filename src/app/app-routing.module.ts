1637e734cd52c18bebe9fbb97d81637bb70fc848
SELVAMANIV22 <venkatesankasima@gmail.com>
2021-07-22 13:42:29 +0530

    commit changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './cart/cart.component';
 import { HeaderComponent } from './header/header.component';
 import { LoginComponent } from './login/login.component';
+import { OrdersComponent } from './orders/orders.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { ResetpasswordComponent } from './resetpassword/resetpassword.component';
 
 const routes: Routes = [
 
   {path:'', redirectTo:'medicare',pathMatch: 'full'},
   {path:'signin' , component: LoginComponent},
+  {path:'newuser' , component: RegistrationComponent},
+  {path:'reset' , component: ResetpasswordComponent},
+  {path:'orders', component:OrdersComponent},
+  {path:'cart' , component: CartComponent},
+  
   
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,10 @@ import { HeaderComponent } from './header/header.component';
 import {MatIconModule} from '@angular/material/icon';
 import { LoginComponent } from './login/login.component';
 import { ResetpasswordComponent } from './resetpassword/resetpassword.component';
+import { HomeComponent } from './home/home.component';
+import { OrdersComponent } from './orders/orders.component';
+import { CartComponent } from './cart/cart.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +20,11 @@ import { ResetpasswordComponent } from './resetpassword/resetpassword.component'
     RegistrationComponent,
     HeaderComponent,
     LoginComponent,
-    ResetpasswordComponent
+    ResetpasswordComponent,
+    HomeComponent,
+    OrdersComponent,
+    CartComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
